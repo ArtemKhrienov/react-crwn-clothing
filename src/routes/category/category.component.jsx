fd04c858a03182baf4b93716fa600ddd9823e929
Artem Khrienov <artemkhrenov@gmail.com>
2022-09-01 05:45:31 -0700

    add redux-thunk to the application and use it for async fetch of the categories

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -2,9 +2,13 @@ import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
-import { selectCategoriesMap } from '../../store/categories/category.selector';
+import {
+  selectCategoriesMap,
+  selectCategoriesIsLoading
+} from '../../store/categories/category.selector';
 
 import ProductCard from '../../components/product-card/product-card.component';
+import Spinner from '../../components/spinner/spinner.component';
 
 import {
   CategoryContainer,
@@ -14,6 +18,7 @@ import {
 const Category = () => {
   const { category } = useParams();
   const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectCategoriesIsLoading);
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
@@ -23,11 +28,17 @@ const Category = () => {
   return (
     <>
       <CategoryTitle>{category.toUpperCase()}</CategoryTitle>
-      <CategoryContainer>
-        {products &&
-          products.map((product) =><ProductCard key={product.id} product={product} />)
-        }
-      </CategoryContainer>
+      {
+        isLoading ? (
+          <Spinner />
+        ) : (
+          <CategoryContainer>
+            {products &&
+              products.map((product) =><ProductCard key={product.id} product={product} />)
+            }
+          </CategoryContainer>
+        )
+      }
     </>
   );
 }
