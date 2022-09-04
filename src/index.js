6ef62b8c9076894705f1c13926038037487baad5
Artem Khrienov <artemkhrenov@gmail.com>
2022-09-04 00:45:09 -0700

    implement servless functions

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider as ReduxProvider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { Elements } from '@stripe/react-stripe-js';
 
+import { stripePromise } from './utils/stripe/stripe.utils';
 import { store, persistor } from './store/store';
 
 import App from './App';
@@ -16,7 +18,9 @@ root.render(
     <ReduxProvider store={store}>
       <PersistGate persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </ReduxProvider>
